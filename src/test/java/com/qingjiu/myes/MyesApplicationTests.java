6307a014949e7b95f914ad15f955618e08d6655a
admin <111>
2020-06-27 22:08:52 +0800

    update

--- src/test/java/com/qingjiu/myes/MyesApplicationTests.java
+++ src/test/java/com/qingjiu/myes/MyesApplicationTests.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.qingjiu.myes.entity.es.ElasticSearchEntity;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
@@ -21,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -86,6 +89,37 @@ public class MyesApplicationTests {
         System.out.println(source);
         System.out.println(index.toString());
         System.out.println(index.status());
+    }
+
+    @Test
+    public void testGetIndex() throws IOException {
+        GetRequest getRequest = new GetRequest("test", "2");
+        GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
+
+        restHighLevelClient.getAsync(getRequest, RequestOptions.DEFAULT, new ActionListener<GetResponse>() {
+            @Override
+            public void onResponse(GetResponse getResponse) {
+
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
+
+        String index = getResponse.getIndex();
+        String id = getResponse.getId();
+        if (getResponse.isExists()) {
+
+            System.out.println(  getResponse.getVersion());
+            System.out.println( getResponse.getSourceAsString());
+            System.out.println( getResponse.getSourceAsMap());
+            System.out.println(getResponse.getSourceAsBytes());
+        } else {
+
+        }
+
     }
 
 
