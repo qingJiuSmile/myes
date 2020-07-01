516124bf38ee96893af2926af8cd41093d023aad
tjy <tjy@weds.com.cn>
2020-07-01 23:06:56 +0800

    update

--- src/main/java/com/qingjiu/myes/service/EsClientUtil.java
+++ src/main/java/com/qingjiu/myes/service/EsClientUtil.java
@@ -1,16 +1,20 @@
 package com.qingjiu.myes.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.rest.RestStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.NotNull;
 import java.io.IOException;
 
 /**
@@ -24,26 +28,36 @@ import java.io.IOException;
 public class EsClientUtil {
 
     @Autowired
-    private RestHighLevelClient restHighLevelClient;
+    @Qualifier("restHighLevelClient")
+    private RestHighLevelClient client;
 
     private long timeOut = 1;
     private long masterTimeOut = 2;
 
 
-    public boolean createIndex(@NotNull String indexName, Long timeOut,
-                               Long masterTimeOut, boolean isAsync) throws IOException {
+    /**
+     * 创建索引
+     *
+     * @param indexName     索引名称
+     * @param timeOut       超时时间
+     * @param masterTimeOut 主节点超时时间
+     * @param isAsync       是否异步执行
+     * @return boolean 是否成功
+     * @author tjy
+     * @date 2020/7/1
+     **/
+    public boolean createIndex(String indexName, Long timeOut, Long masterTimeOut, boolean isAsync) throws IOException {
         // 创建索引请求
         CreateIndexRequest request = new CreateIndexRequest(indexName);
-        CreateIndexResponse response = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
-
         // 超时时间
         request.setTimeout(TimeValue.timeValueMinutes(timeOut == null ? this.timeOut : timeOut));
         // 主节点超时时间
         request.setMasterTimeout(TimeValue.timeValueMinutes(masterTimeOut == null ? this.masterTimeOut : masterTimeOut));
+        CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
 
         // 异步添加索引 todo 这里异步会抛异常，但是可以成功添加
         if (isAsync) {
-            restHighLevelClient.indices().createAsync(request, RequestOptions.DEFAULT, new ActionListener<CreateIndexResponse>() {
+            client.indices().createAsync(request, RequestOptions.DEFAULT, new ActionListener<CreateIndexResponse>() {
                 @Override
                 public void onResponse(CreateIndexResponse createIndexResponse) {
                     log.info("createIndexResponse ==> [{}]", createIndexResponse.isAcknowledged());
@@ -51,7 +65,7 @@ public class EsClientUtil {
 
                 @Override
                 public void onFailure(Exception e) {
-                    log.error(e.getMessage(),e);
+                    log.error(e.getMessage(), e);
                 }
             });
         }
@@ -63,5 +77,56 @@ public class EsClientUtil {
         // boolean shardsAcknowledged = response.isShardsAcknowledged();
 
         return acknowledged;
+    }
+
+
+    /**
+     *  删除索引
+     * @param indexName     索引名称
+     * @param timeOut       超时时间
+     * @param masterTimeOut 主节点超时时间
+     * @param isAsync       是否异步执行
+     * @author tjy
+     * @date 2020/7/1
+     * @return boolean 是否成功
+     **/
+    public boolean deleteIndex(String indexName, Long timeOut, Long masterTimeOut, boolean isAsync) throws IOException {
+
+        try {
+            // 创建删除索引请求
+            DeleteIndexRequest request = new DeleteIndexRequest(indexName);
+            // 超时时间
+            request.timeout(TimeValue.timeValueMinutes(timeOut == null ? this.timeOut : timeOut));
+            // 主节点超时时间
+            request.masterNodeTimeout(TimeValue.timeValueMinutes(masterTimeOut == null ? this.masterTimeOut : masterTimeOut));
+            AcknowledgedResponse deleteIndexResponse = client.indices().delete(request, RequestOptions.DEFAULT);
+
+            if (isAsync) {
+
+                client.indices().deleteAsync(request, RequestOptions.DEFAULT, new ActionListener<AcknowledgedResponse>() {
+                    @Override
+                    public void onResponse(AcknowledgedResponse acknowledgedResponse) {
+                        log.info("AcknowledgedResponse ==> [{}]", acknowledgedResponse.isAcknowledged());
+
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        log.error(e.getMessage(), e);
+                    }
+                });
+            }
+
+            return deleteIndexResponse.isAcknowledged();
+
+        }catch (ElasticsearchException exception){
+            if (exception.status() == RestStatus.NOT_FOUND) {
+                 log.error("删除失败，找不到对应索引");
+                 log.error(exception.getMessage(),exception);
+            }else {
+                log.error(exception.getMessage(),exception);
+            }
+        }
+      return false;
     }
 }
